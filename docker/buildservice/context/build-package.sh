093db9a49700ca5a59050efb94830b936168a84f
Philipp Naderer <philipp.naderer@gmail.com>
2016-06-20 22:45:16 +0200

    Fixes path to ringo binary

--- docker/buildservice/context/build-package.sh
+++ docker/buildservice/context/build-package.sh
@@ -16,7 +16,7 @@ cp /opt/*.deb /opt/out
 echo "Copied files"
 
 # checksums
-RINGO_VERSION=`ringo -e "const engine = require('ringo/engine'); print(engine.version.join('.'));"`
+RINGO_VERSION=`/opt/ringojs/bin/ringo -e "const engine = require('ringo/engine'); print(engine.version.join('.'));"`
 cd /opt/out
 sha256sum ringojs* 1> SHASUMS256-`echo $RINGO_VERSION`.txt
 echo "Generated SHA-256 checksums"
